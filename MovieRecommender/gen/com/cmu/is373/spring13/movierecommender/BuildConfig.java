2f50bd64861a9aede36e4d8ecbc208b0e466e0d0
Rob Rua <rrua@andrew.cmu.edu>
2013-02-07 18:52:41 -0500

    Default Android app

---
+++ MovieRecommender/gen/com/cmu/is373/spring13/movierecommender/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.cmu.is373.spring13.movierecommender;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
